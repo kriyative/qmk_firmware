81f6af68bb3159e1b39bebc45eba1f16d93cb37f
Ram Krishnan <kriyative@gmail.com>
2018-09-19 21:25:15 -0700

    Keymap: adds ESC to default layer

--- keyboards/atreus62/keymaps/kriyative/keymap.c
+++ keyboards/atreus62/keymaps/kriyative/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------||      ||      ||------+------+------+------+------+------|
    * |Shift |   Z  |   X  |   C  |   V  |   B  || Alt  || Ctrl ||   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------||      ||      ||------+------+------+------+------+------|
-   * |      | Ctrl |Super |  Alt |  Fn  | Bksp |`------'`------'|Space |  Fn  | Alt  |Super |   =  |Enter |
+   * | ESC  | Ctrl |Super |  Alt |  Fn  | Bksp |`------'`------'|Space |  Fn  | Alt  |Super |   =  |Enter |
    * `-----------------------------------------'                `-----------------------------------------'
    */
   [_DEFAULT] = LAYOUT( /* qwerty */
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    _______,KC_LCTRL, KC_LGUI, KC_LALT, FN,      KC_BSPC, KC_LALT, KC_RCTRL,  KC_SPC,  FN,      KC_RALT, KC_RGUI, KC_EQL,  KC_ENT
+    KC_ESC,  KC_LCTRL,KC_LGUI, KC_LALT, FN,      KC_BSPC, KC_LALT, KC_RCTRL,  KC_SPC,  FN,      KC_RALT, KC_RGUI, KC_EQL,  KC_ENT
   ),
 
   /* Function layer
